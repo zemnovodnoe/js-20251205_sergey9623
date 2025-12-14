f2c74e7374d317b2d85dc996ce9e73abead9639f
Sergey Egorov <udav81@gmail.com>
2025-12-14 21:08:04 +0300

    invertObj() 2 variants

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,15 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (typeof obj != 'object') {return undefined;}
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [value, key])
+  );
+}
 
+export function invertObj1(obj) {
+  if (typeof obj != 'object') {return undefined;}
+  let ret = {};
+  Object.keys(obj).forEach(key => ret[obj[key]] = key);
+  return ret;
 }
