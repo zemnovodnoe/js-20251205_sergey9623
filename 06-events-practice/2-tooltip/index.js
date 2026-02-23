6d262d38f55ea9db764b5c3cf4f6844f8b4c1fc0
Sergey Egorov <udav81@gmail.com>
2026-02-24 00:41:29 +0300

    06-tooltip

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,63 @@
 class Tooltip {
-  initialize () {
+  static obj = null;
+  element = null;
+  constructor() {
+    if (!Tooltip.obj) {
+      Tooltip.obj = this;
+    }
+    return Tooltip.obj;
+  }
 
+  initialize() {
+    if (!this.element) {
+      this.element = document.createElement('div');
+      this.element.className = "tooltip";
+      document.body.append(this.element);
+      document.addEventListener('pointerover', (event) => { this.onMouseIn(event); });
+    }
+  }
+
+  onMouseIn(event) {
+    if (event.target.dataset.tooltip) {
+      this.render(event.target.dataset.tooltip);
+      document.addEventListener('mousemove', (event) => { this.onMouseMove(event); });
+      document.addEventListener('pointerout', (event) => { this.onMouseOut(event); });
+    }
+  }
+
+  onMouseMove(event) {
+    if (this.element) {
+      this.element.style.left = event.clientX + "px";
+      this.element.style.top = event.clientY + "px";
+    }
+  }
+
+  onMouseOut(_) {
+    if (this.element) {
+      removeEventListener('pointerout', this.onMouseOut);
+      removeEventListener('mousemove', this.onMouseMove);
+      this.destroy();
+    }
+  }
+
+  render(text) {
+    if (!this.element) {
+      this.element = document.createElement('div');
+      this.element.className = "tooltip";
+      document.body.append(this.element);
+    }
+    this.element.innerHTML = text;
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
   }
 }
 
